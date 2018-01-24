8cbc53431862806c9f371034069330daf19fbdaa
Keremcan Çakar <keremcan@hotmail.com.tr>
2018-01-24 22:47:53 +0300

    Fixed target frameworks

--- Law/Law.Admin/Controllers/HomeController.cs
+++ Law/Law.Admin/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Law.Admin.Models;
 
-namespace Admin.Controllers
+namespace Law.Admin.Controllers
 {
     public class HomeController : Controller
     {
@@ -29,7 +31,7 @@ namespace Admin.Controllers
 
         public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
