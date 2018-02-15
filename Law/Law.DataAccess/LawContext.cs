a07875e4ad90382b76b14b78a035c435bb406fbd
Osman POLAT <polatosmann@gmail.com>
2018-02-16 00:35:08 +0300

    entityframeworkcore entegrasyon calismasi

---
+++ Law/Law.DataAccess/LawContext.cs
@@ -0,0 +1,29 @@
+﻿using Law.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Law.DataAccess
+{
+    public class LawContext : DbContext
+    {
+
+        public LawContext(DbContextOptions<LawContext> options) : base(options)
+        {
+           
+        }
+
+        public DbSet<Affiliate> Affiliates { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<ArticlePiece> ArticlePieces { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Contributor> Contributors { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<NameBase> NameBases { get; set; }  
+        public DbSet<PracticeArea> PracticeAreas { get; set; }pa
+        public DbSet<User> Users { get; set; }
+        
+
+    }
+}
